f5a611b12d0bb872144ac42dd2507b3244baf571
Meenalkhare2727 <meenalkhare027@gmail.com>
2024-04-06 00:02:33 +0530

    MCIP ASSIGNMENTS

---
+++ I2c_uart_assignment/Inc/tim7.h
@@ -0,0 +1,15 @@
+
+
+#ifndef TIM7_H_
+#define TIM7_H_
+
+#include "stm32f4xx.h"
+#include  "stm32f407xx.h"
+
+#define TCLK		16000000
+#define TIM_PR		16000
+
+void TimerInit(void);
+void TimerDelayMs(uint32_t ms);
+
+#endif /* TIM7_H_ */
---
+++ demo13_assignment1/Src/timer.c
@@ -0,0 +1,15 @@
+/*
+ * timer.c
+ *
+ *  Created on: Apr 4, 2024
+ *      Author: meenal
+ */
+
+
+#include "timer.h"
+
+void TimerInit(void){
+
+	RCC->APB1ENR |=RCC_APB1ENR_TIME
+
+}
